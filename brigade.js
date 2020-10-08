13954081493defa2b48f32cf26371f10bcc16f8b
Santiago Yepes <zetogk@gmail.com>
2020-10-08 16:37:40 -0500

    1

--- brigade.js
+++ brigade.js
@@ -2,6 +2,16 @@ const { events, Job } = require('brigadier');
 
 events.on('push', (e, p) => {
 
-    console.log(`NEW PUSH!!! ${e}`);
+    console.log(`NEW PUSH!!! ${JSON.stringify(e)}`);
 
-})
+    const jobTesting = new Job('job-testing', 'node:12-alpine');
+    jobTesting.tasks = [
+        'cd /src',
+        'npm install',
+        'npm run test'
+    ];
+    jobTesting.streamLogs = true;
+    jobTesting.run();
+
+});
+
